bc2ae254fb0fd65ada7019c4da15d2b411f2c866
gonzalo <gonzalogarciadev@gmail.com>
2022-11-12 12:03:20 -0500

    Add exercise 1.4

--- part1/courseinfo/src/Components/Total.js
+++ part1/courseinfo/src/Components/Total.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-const Total = ({ part1, part2, part3 }) => {
-  return (<p>Number of exercises {part1.exercises + part2.exercises + part3.exercises}</p>)
+const Total = ({ parts }) => {
+  return (<p>Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>)
 };
 
 export default Total;
