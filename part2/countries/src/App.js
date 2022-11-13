a02c56d611746a95c0da434d708317084b871f8f
gonzalo <gonzalogarciadev@gmail.com>
2022-11-13 14:10:54 -0500

    Add exercise 2.13

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,72 +1,71 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
+import { useState, useEffect } from 'react'
+import Country from './Components/Country'
+import axios from 'axios'
 
 const App = () => {
-  const [countries, setCountries] = useState([]);
-  const [filter, setFilter] = useState("");
 
-  useEffect(() => {
-    axios.get("https://restcountries.com/v3.1/all").then((response) => {
-      setCountries(response.data);
-    });
-  }, []);
+  const [countries, setCountries] = useState([])
+  const [filter, setFilter] = useState('')
+  const [ flagArr, setFlagArr ] = useState({})
+  const [flag, setFlag] = useState(false)
+  
+  useEffect(()=>{
+    axios
+      .get('https://restcountries.com/v3.1/all')
+      .then(response => {
+        setCountries(response.data)})
+  }, [])
 
-  const countriesToShow = countries.filter((country) =>
-    country.name.common.toLowerCase().includes(filter)
-  );
 
-  const handleFilter = (event) => {
-    setFilter(event.target.value);
-  };
+  const countriesToShow =  
+    countries.filter(country => 
+      country.name.common.toLowerCase().includes(filter) )
+
+
+  const handleFilter = (event) =>{
+    setFilter(event.target.value)
+  }
 
   return (
-    <div
-      style={{
-        maxWidth: "400px",
-        margin: "0 auto",
-        minHeight: "auto",
-        padding: "20px",
-        border: "1px solid black",
-        borderRadius: "5px",
-      }}
-    >
+    <div style={{
+      'maxWidth': '400px',
+      'margin': '0 auto',
+      'minHeight': 'auto',
+      'padding': '20px',
+      'border' : '1px solid black',
+      'borderRadius': '5px'
+    }}>
       <h2>Countries</h2>
-      <label>
-        <span style={{ marginRight: "10px" }}>filter shown with</span>
-        <input value={filter} onChange={handleFilter} />
-      </label>
+        <label>
+          <span style={{'marginRight': '10px'}}>
+            filter shown with 
+          </span>
+          <input value={filter} onChange={handleFilter} />
+        </label>
       <h2>Results</h2>
-
-      {countries.length !== 0 ? (
-        countriesToShow.length <= 10 ? (
-          countriesToShow.length !== 1 ? (
-            <ul>
-              {countriesToShow.map((country) => (
-                <li key={country.cca2}>{country.name.common}</li>
-              ))}
-            </ul>
-          ) : (
-            <div style={{ textAlign: "center" }}>
-              <h1>{countriesToShow[0].name.common}</h1>
-              <p>Capital: {countriesToShow[0].capital}</p>
-              <p>Area: {countriesToShow[0].area}</p>
-              <h3>languages: </h3>
-              <ul>
-                {Object.keys(countriesToShow[0].languages).map((item) => (
-                  <li key={item}>{countriesToShow[0].languages[item]}</li>
-                ))}
-              </ul>
-              <img src={countriesToShow[0].flags.png} alt="flag"/>
-            </div>
-          )
-        ) : (
-          <div>There are too many matches</div>
-        )
-      ) : (
-        <div>Waiting...</div>
-      )}
+      
+        {countries.length !== 0
+        ? countriesToShow.length <= 10
+            ? countriesToShow.length !== 1
+              ? <ul>{countriesToShow.map((country, index) =>{
+                 console.log('setting', index, 'to', flagArr)
+                 return <li key={country.cca2}>
+                  {country.name.common} &nbsp;
+                  <button onClick={()=> setFlagArr({ ...flagArr, [index]: !flagArr[index] }) }>
+                    Show</button>
+                  {flagArr[index]
+                    ? <Country country={countriesToShow[index]} />
+                    : <></>
+                  }
+                  </li>}
+                 )}
+                 </ul>
+              : <Country country={countriesToShow[0]} />
+            : <div>There are too many matches</div>
+        : <div>Waiting...</div>}
+      
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
---
+++ part2/countries/src/Components/Country.js
@@ -0,0 +1,18 @@
+const Country = ({ country }) => {
+    return (
+      <div style={{'textAlign': 'center'}}>
+                  <h1>{country.name.common}</h1>
+                  <p>Capital: {country.capital}</p>
+                  <p>Area: {country.area}</p>
+                  <h3>languages: </h3>
+                  <ul>
+                    {Object.keys(country.languages).map(item =>
+                    <li key={item}>{country.languages[item]}</li>
+                      )}
+                  </ul>
+                  <img src={country.flags.png} alt="flag"/>
+      </div>
+    )
+  }
+  
+export default Country
