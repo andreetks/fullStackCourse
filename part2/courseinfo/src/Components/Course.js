b4341cdeaba985282c477a0ae2b6613f9b92dd1a
gonzalo <gonzalogarciadev@gmail.com>
2022-11-13 13:16:47 -0500

    Add exercise 2.3

--- part2/courseinfo/src/Components/Course.js
+++ part2/courseinfo/src/Components/Course.js
@@ -4,11 +4,16 @@ import Content from "./Content";
 import Total from "./Total";
 
 const Course = ({ course }) => {
+  const total = course.parts.reduce((s, p) => {
+    console.log(`this is s -> ${s}  && this is p -> ${p.exercises}`);
+    return s + p.exercises;
+  }, 0);
+
   return (
     <div>
       <Header course={course.name} />
       <Content parts={course.parts} />
-      <Total parts={course.parts} />
+      <Total total={total} />
     </div>
   );
 };
