d26fbb386382648419d1892271062a39b65fd082
gonzalo <gonzalogarciadev@gmail.com>
2022-11-13 13:29:40 -0500

    Add exercise 2.5

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,5 +1,7 @@
 import Course from "./Components/Course";
 
+// The exercise 2.5 is already satisfied
+
 const App = () => {
   const courses = [
     {
@@ -45,32 +47,6 @@ const App = () => {
       ],
     },
   ];
-  // const course = {
-  //   id: 1,
-  //   name: 'Half Stack application development',
-  //   parts: [
-  //     {
-  //       name: 'Fundamentals of React',
-  //       exercises: 10,
-  //       id: 1
-  //     },
-  //     {
-  //       name: 'Using props to pass data',
-  //       exercises: 7,
-  //       id: 2
-  //     },
-  //     {
-  //       name: 'State of a component',
-  //       exercises: 14,
-  //       id: 3
-  //     },
-  //     {
-  //       name: 'Redux',
-  //       exercises: 11,
-  //       id: 4
-  //     }
-  //   ]
-  // }
 
   return (
     <>
