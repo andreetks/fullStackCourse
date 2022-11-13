34450c87f93afd7877073b1750e807fce1b077b5
gonzalo <gonzalogarciadev@gmail.com>
2022-11-13 14:13:22 -0500

    Add exercise 2.14

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,70 +1,83 @@
-import { useState, useEffect } from 'react'
-import Country from './Components/Country'
-import axios from 'axios'
+import { useState, useEffect } from "react";
+import Country from "./components/Country";
+import axios from "axios";
 
 const App = () => {
+  const [countries, setCountries] = useState([]);
+  const [filter, setFilter] = useState("");
+  const [flagArr, setFlagArr] = useState({});
 
-  const [countries, setCountries] = useState([])
-  const [filter, setFilter] = useState('')
-  const [ flagArr, setFlagArr ] = useState({})
-  
-  useEffect(()=>{
-    axios
-      .get('https://restcountries.com/v3.1/all')
-      .then(response => {
-        setCountries(response.data)})
-  }, [])
+  useEffect(() => {
+    axios.get("https://restcountries.com/v3.1/all").then((response) => {
+      setCountries(response.data);
+    });
+  }, []);
 
+  const countriesToShow = countries.filter((country) =>
+    country.name.common.toLowerCase().includes(filter.toLowerCase())
+  );
 
-  const countriesToShow =  
-    countries.filter(country => 
-      country.name.common.toLowerCase().includes(filter) )
-
-
-  const handleFilter = (event) =>{
-    setFilter(event.target.value)
-  }
+  const handleFilter = (event) => {
+    setFilter(event.target.value);
+  };
 
   return (
-    <div style={{
-      'maxWidth': '400px',
-      'margin': '0 auto',
-      'minHeight': 'auto',
-      'padding': '20px',
-      'border' : '1px solid black',
-      'borderRadius': '5px'
-    }}>
+    <div
+      style={{
+        maxWidth: "400px",
+        margin: "0 auto",
+        minHeight: "auto",
+        padding: "20px",
+        border: "1px solid black",
+        borderRadius: "5px",
+      }}
+    >
       <h2>Countries</h2>
-        <label>
-          <span style={{'marginRight': '10px'}}>
-            filter shown with 
-          </span>
-          <input value={filter} onChange={handleFilter} />
-        </label>
+      <label>
+        <span style={{ marginRight: "10px" }}>filter countries by:</span>
+        <input value={filter} onChange={handleFilter} />
+      </label>
       <h2>Results</h2>
-      
-        {countries.length !== 0
-        ? countriesToShow.length <= 10
-            ? countriesToShow.length !== 1
-              ? <ul>{countriesToShow.map((country, index) =>{
-                 console.log('setting', index, 'to', flagArr)
-                 return <li key={country.cca2}>
-                  {country.name.common} &nbsp;
-                  <button onClick={()=> setFlagArr({ ...flagArr, [index]: !flagArr[index] }) }>
-                    Show</button>
-                  {flagArr[index]
-                    ? <Country country={countriesToShow[index]} />
-                    : <></>
-                  }
-                  </li>}
-                 )}
-                 </ul>
-              : <Country country={countriesToShow[0]} />
-            : <div>There are too many matches</div>
-        : <div>Waiting...</div>}
-      
-    </div>
-  )
-}
 
-export default App
+      {countries.length !== 0 ? (
+        countriesToShow.length <= 10 ? (
+          countriesToShow.length !== 1 ? (
+            countriesToShow.length !== 0 ? (
+              <ul>
+                {countriesToShow.map((country, index) => {
+                  return (
+                    <li key={country.cca2}>
+                      {country.name.common} &nbsp;
+                      <button
+                        onClick={() =>
+                          setFlagArr({ ...flagArr, [index]: !flagArr[index] })
+                        }
+                      >
+                        Show
+                      </button>
+                      {flagArr[index] ? (
+                        <Country country={countriesToShow[index]} />
+                      ) : (
+                        <></>
+                      )}
+                    </li>
+                  );
+                })}
+              </ul>
+            ) : (
+              <div>There aren't matches</div>
+            )
+          ) : (
+            <Country country={countriesToShow[0]} />
+          )
+        ) : (
+          <div>There are too many matches</div>
+        )
+      ) : (
+        <div>Waiting...</div>
+      )}
+    </div>
+  );
+};
+
+export default App;
--- part2/countries/src/Components/Country.js
+++ part2/countries/src/Components/Country.js
@@ -1,18 +1,53 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+
 const Country = ({ country }) => {
-    return (
-      <div style={{'textAlign': 'center'}}>
-                  <h1>{country.name.common}</h1>
-                  <p>Capital: {country.capital}</p>
-                  <p>Area: {country.area}</p>
-                  <h3>languages: </h3>
-                  <ul>
-                    {Object.keys(country.languages).map(item =>
-                    <li key={item}>{country.languages[item]}</li>
-                      )}
-                  </ul>
-                  <img src={country.flags.png} alt={country.name.common + ' flag'} />
-      </div>
-    )
-  }
-  
-export default Country
+  const weather_key = process.env.REACT_APP_WEATHER_KEY;
+  const [weather, setWeather] = useState([]);
+  const url =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    country.capital +
+    "&appid=" +
+    weather_key +
+    "&units=metric";
+
+  useEffect(() => {
+    axios.get(url).then((response) => {
+      setWeather(response.data);
+    });
+  }, []);
+
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h1>{country.name.common}</h1>
+      <p>Capital: {country.capital}</p>
+      <p>Area: {country.area}</p>
+      <h3>languages: </h3>
+      <ul>
+        {Object.keys(country.languages).map((item) => (
+          <li key={item}>{country.languages[item]}</li>
+        ))}
+      </ul>
+      <img src={country.flags.png} alt={country.name.common + " flag"} />
+      <h3>Weather in {country.capital}</h3>
+      {weather.length === 0 ? (
+        <div>No weather</div>
+      ) : (
+        <div>
+          <p>temperature {weather.main.temp}° Celcius</p>
+          <img
+            alt={country.name.common + " weather icon"}
+            src={
+              "http://openweathermap.org/img/wn/" +
+              weather.weather[0].icon +
+              "@2x.png"
+            }
+          />
+          <p>wind {weather.wind.speed} m/s</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Country;
