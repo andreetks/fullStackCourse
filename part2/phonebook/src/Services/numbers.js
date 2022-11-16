74781d07148c114d5966443b2d19deea75439eeb
gonzalo <gonzalogarciadev@gmail.com>
2022-11-16 16:46:13 -0500

    change url

--- part2/phonebook/src/Services/numbers.js
+++ part2/phonebook/src/Services/numbers.js
@@ -1,19 +1,19 @@
 import axios from "axios";
-const baseUrl = "http://localhost:3001/api/persons";
+const baseUrl = "https://lionfish-app-cpxa4.ondigitalocean.app/api/persons";
 
 const getAll = () => {
   const req = axios.get(baseUrl);
-  return req.then((response) => response.data);
+  return req.then((response) => response.data).catch((error) => error);
 };
 
 const create = (newObject) => {
   const req = axios.post(baseUrl, newObject);
-  return req.then((response) => response.data);
+  return req.then((response) => response.data).catch((error) => error);
 };
 
 const update = (id, newObject) => {
   const req = axios.put(`${baseUrl}/${id}`, newObject);
-  return req.then((response) => response.data);
+  return req.then((response) => response.data).catch((error) => error);
 };
 
 const remove = (id) => {
